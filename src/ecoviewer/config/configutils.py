4f288f460743e8eae50d7e6dba68534b85a4fe72
Nolan Price <nolan.price@gmail.com>
2024-03-27 11:21:29 -0700

    whoops

--- src/ecoviewer/config/configutils.py
+++ src/ecoviewer/config/configutils.py
@@ -99,3 +99,82 @@ def get_organized_mapping(df_columns, graph_df : pd.DataFrame, field_df : pd.Dat
                 "y2_fields" : y2_fields
             }
     return returnDict
+
+def generate_summary_query(day_table, numDays = 7, start_date = None, end_date = None):
+    summary_query = f"SELECT * FROM {day_table} "
+    if start_date != None and end_date != None:
+        summary_query += f"WHERE time_pt >= '{start_date}' AND time_pt <= '{end_date} 23:59:59' ORDER BY time_pt ASC"
+    else:
+        summary_query += f"ORDER BY time_pt DESC LIMIT {numDays}" #get last x days
+        summary_query = f"SELECT * FROM ({summary_query}) AS subquery ORDER BY subquery.time_pt ASC;"
+    return summary_query
+
+def generate_hourly_summary_query(hour_table, day_table, numHours = 190, load_shift_tracking = True, start_date = None, end_date = None):
+    if load_shift_tracking:
+        hourly_summary_query = f"SELECT {hour_table}.*, HOUR({hour_table}.time_pt) AS hr, {day_table}.load_shift_day FROM {hour_table} " +\
+            f"LEFT JOIN {day_table} ON {day_table}.time_pt = {hour_table}.time_pt "
+    else:
+        hourly_summary_query = f"SELECT {hour_table}.*, HOUR({hour_table}.time_pt) AS hr FROM {hour_table} "
+    if start_date != None and end_date != None:
+        hourly_summary_query += f"WHERE {hour_table}.time_pt >= '{start_date}' AND {hour_table}.time_pt <= '{end_date} 23:59:59' ORDER BY time_pt ASC"
+    else:
+        hourly_summary_query += f"ORDER BY {hour_table}.time_pt DESC LIMIT {numHours}" #get last 30 days plus some 740
+        hourly_summary_query = f"SELECT * FROM ({hourly_summary_query}) AS subquery ORDER BY subquery.time_pt ASC;"
+
+    return hourly_summary_query
+
+def generate_raw_data_query(min_table, hour_table, day_table, field_df, selected_table, state_tracking = True, start_date = None, end_date = None):
+    query = f"SELECT {min_table}.*, "
+    if state_tracking:
+        query += f"{hour_table}.system_state, "
+    
+    # conditionals because some sites don't have these
+    if field_df[(field_df['field_name'] == 'OAT_NOAA') & (field_df['site_name'] == selected_table)].shape[0] > 0:
+        query += f"{hour_table}.OAT_NOAA, "
+    if field_df[(field_df['field_name'] == 'COP_Equipment') & (field_df['site_name'] == selected_table)].shape[0] > 0:
+        query += f"{day_table}.COP_Equipment, "
+    if field_df[(field_df['field_name'] == 'COP_DHWSys_2') & (field_df['site_name'] == selected_table)].shape[0] > 0:
+        query += f"{day_table}.COP_DHWSys_2, "
+    query += f"IF(DAYOFWEEK({min_table}.time_pt) IN (1, 7), FALSE, TRUE) AS weekday, " +\
+        f"HOUR({min_table}.time_pt) AS hr FROM {min_table} "
+    #TODO these two if statements are a work around for LBNLC. MAybe figure out better solution
+    if min_table != hour_table:
+        query += f"LEFT JOIN {hour_table} ON {min_table}.time_pt = {hour_table}.time_pt "
+    if min_table != day_table:
+        query += f"LEFT JOIN {day_table} ON {min_table}.time_pt = {day_table}.time_pt "
+
+    if start_date != None and end_date != None:
+        query += f"WHERE {min_table}.time_pt >= '{start_date}' AND {min_table}.time_pt <= '{end_date} 23:59:59' ORDER BY {min_table}.time_pt ASC"
+    else:
+        query += f"ORDER BY {min_table}.time_pt DESC LIMIT 4000"
+        query = f"SELECT * FROM ({query}) AS subquery ORDER BY subquery.time_pt ASC;"
+
+    return query
+
+def log_event(user_email, selected_table, start_date, end_date, sql_dash_config):
+    cnx = mysql.connector.connect(**sql_dash_config)
+    cursor = cnx.cursor() 
+
+    fields = ['event_time', 'email_address']
+    formated_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    values = [f'"{formated_date}"', f'"{user_email}"']
+
+    if not selected_table is None:
+        fields.append('site_name')
+        values.append(f'"{selected_table}"')
+    if not start_date is None:
+        fields.append('start_date')
+        values.append(f'"{start_date}"')
+    if not end_date is None:
+        fields.append('end_date')
+        values.append(f'"{end_date}"')
+
+    insert_query = f"INSERT INTO dash_activity_log ({', '.join(fields)}) VALUES ({', '.join(values)});"
+    print(insert_query)
+
+    cursor.execute(insert_query)
+    
+    # Commit the changes
+    cnx.commit()
+    cursor.close()
+    cnx.close()
