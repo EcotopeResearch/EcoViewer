6d8d7f37eccb632088d9ee9c6ac91f2c21b19bf1
pricen3 <nolan.price@gmail.com>
2024-11-14 13:58:52 -0800

    minor filtering tweak for cop timeseries

--- src/ecoviewer/objects/GraphObject/graphs/COPTimeseries.py
+++ src/ecoviewer/objects/GraphObject/graphs/COPTimeseries.py
@@ -5,12 +5,12 @@ from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
 class COPTimeseries(GraphObject):
-    def __init__(self, dm : DataManager, title : str = "COP Timeseries", summary_group : str = None):
+    def __init__(self, dm : DataManager, title : str = "System COP Timeseries", summary_group : str = None):
         self.summary_group = summary_group
         super().__init__(dm, title)
 
     def create_graph(self, dm : DataManager):
-        df_daily = dm.get_daily_data_df(events_to_filter=['EQUIPMENT_MALFUNCTION','DATA_LOSS_COP'])
+        df_daily = dm.get_daily_data_df(events_to_filter=['DATA_LOSS_COP'])
         if not 'Temp_OutdoorAir' in df_daily.columns:
             if not dm.oat_variable in df_daily.columns:
                 raise Exception('No outdoor air temperature data available.')
@@ -31,7 +31,7 @@ class COPTimeseries(GraphObject):
                                 mode = 'markers', name = 'City Water Temperature',
                                 marker=dict(color='darkblue')), secondary_y = False)
 
-        fig.update_layout(title = '<b>System COP')
+        fig.update_layout(title = f'<b>{self.title}')
         fig.update_xaxes(title = '<b>Date')
         fig.update_yaxes(title = '<b>System COP', secondary_y = True)
         fig.update_yaxes(title = '<b>Daily Average Air and Water Temperature (F)', secondary_y = False)
